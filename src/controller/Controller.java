b8c914c251346850793e5c0dc44bafca49d54562
hyunn522 <tjgus52213@gmail.com>
2025-07-04 19:09:46 +0900

    피드백 반영

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import View.FinishView;
 import View.QuizView;
@@ -9,7 +8,7 @@ import View.StartView;
 import Model.Model;
 
 public class Controller {
-	private static String name; 
+	private static String name;  //사람 이름
 	private static ArrayList<String> saveAnswerList;
 	
 	private static final Model model = Model.getModel();
@@ -23,16 +22,12 @@ public class Controller {
 	
 	// 입력값 저장
 	public static void saveAnswerList(ArrayList<String> dataList) {
-//		String[] list = dataList.toArray(new String[dataList.size()]);
-//		int size=0;
-//		for(String data : dataList){
-//			list[size++] = data;
-//		}
-//		
+	
 		model.saveScore(dataList);
 		saveAnswerList = model.getResult();
+		int count = saveAnswerList.size();
 		
-		if (saveAnswerList.size() == 0) {
+		if (count == 0) {
 			System.out.println("적절한 N잡을 찾지 못했어요 ㅜㅜ");
 			System.exit(0);;
 		}
@@ -40,16 +35,19 @@ public class Controller {
 		System.out.println("-------------------------");
 		System.out.println("결과는~~~~~~???!");
 		
-		if(saveAnswerList.size() == 0) {
+		
+		if(count == 0) {
 			//TODO: throw 추가 예
 		}else {
 			// 결과값 출력
 			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < saveAnswerList.size(); i++) {
-				if (i < saveAnswerList.size() - 1) sb.append(saveAnswerList.get(i) + ", ");
-				else sb.append(saveAnswerList.get(i) + "입니다!");
+			for (int i = 0; i < count; i++) {
+				if (i < count - 1) {
+					sb.append(saveAnswerList.get(i) + ", ");
+				}else {
+					sb.append(saveAnswerList.get(i) + "입니다!");
+				}
 			}
-//			FinishView.print(name+" 님의 결과는~~?? 따따딸ㄴ~~~ " + saveAnswerList);
 			FinishView.print(name+"님에게 어울리는 N잡은 " + sb);
 			
 		}
@@ -58,20 +56,20 @@ public class Controller {
 	}
 	
 	
-	//이름 세팅
+	//이름 세팅  "" : length() 
 	public static void setName(String n) throws Exception {
-		name = n;
-//		System.out.println("controller !!");
-//		System.out.println(name);
 		
-		if(n.length() == 0) {
+		name = n;
+		
+		if(n == null || n.length() == 0) {
 			System.out.println("아 해줘.. 이름 내놔 뭐야?");
 			StartView.setName();
-		}
-		else {
+		}else {
 			System.out.println("퀴즈 쓰따뚜");
 			model.setPerson(name);
 			QuizView.startQuiz(model.getQuestions());
 		}
+		
 	}
+	
 }
