83f2d66dc0c72725c58787e00e07e72ff5b3e715
dlacowns21 <dlacowns21@naver.com>
2025-07-04 15:00:21 +0900

    feat/model 구현 완료

--- src/Model/domain/Person.java
+++ src/Model/domain/Person.java
@@ -1,5 +1,16 @@
 package Model.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Person {
+	private String name;
+	private int score;
 
 }
--- src/Model/domain/Question.java
+++ src/Model/domain/Question.java
@@ -1,5 +1,17 @@
 package Model.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Question {
+	
+	private String category;
+	private String text;
 
 }
