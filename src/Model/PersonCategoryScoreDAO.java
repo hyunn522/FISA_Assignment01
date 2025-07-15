c01eb9c0e9d554474e251d9a63ddce92131d6507
hyunn522 <tjgus52213@gmail.com>
2025-07-15 15:49:42 +0900

    feat: DB에 유저의 계산된 점수 저장 메서드 추가

--- src/Model/PersonCategoryScoreDAO.java
+++ src/Model/PersonCategoryScoreDAO.java
@@ -1,19 +1,49 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-import Model.domain.Answer;
+import Model.domain.PersonCategoryScore;
+import Util.DBUtil;
 
 public class PersonCategoryScoreDAO {
-	private PersonCategoryScoreDAO personCategoryScoreDAO = new PersonCategoryScoreDAO();
+	
+	private static PersonCategoryScoreDAO personCategoryScoreDAO = new PersonCategoryScoreDAO();
 	
 	private PersonCategoryScoreDAO() {}
 	
-	public PersonCategoryScoreDAO getPersonCategoryScoreDAO() {
+	public static PersonCategoryScoreDAO getPersonCategoryScoreDAO() {
 		return personCategoryScoreDAO;
 	}
 	
-	public ArrayList<Answer> getAnswers(){
-		return null;
+	// 각 카테고리별 사람 정보 저장
+	public boolean getAnswers(int personId, ArrayList<PersonCategoryScore> scores) throws SQLException{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+	        conn = DBUtil.getConnection();
+	        pstmt = conn.prepareStatement("INSERT INTO personcategoryscore (personid, category, score) VALUES (?, ?, ?)");
+	        int resultCnt = 0;
+	        
+	        for (PersonCategoryScore score : scores) {
+	            pstmt.setInt(1, personId);
+	            pstmt.setString(2, score.getCategory());
+	            pstmt.setInt(3, score.getScore());
+	            resultCnt += pstmt.executeUpdate();
+	        }
+
+	        if (resultCnt == 4) {
+	        	return true;
+	        }
+		} finally {
+			DBUtil.close(conn, pstmt, rs);
+		}
+		
+		return false;
 	}
 }
