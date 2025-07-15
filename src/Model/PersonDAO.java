8f793e94801f737f3e927e56901810249d0352be
hyunn522 <tjgus52213@gmail.com>
2025-07-15 15:27:26 +0900

    feat: DB에 유저 저장 및 불러오는 메서드 추가

--- src/Model/PersonDAO.java
+++ src/Model/PersonDAO.java
@@ -1,24 +1,65 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import Model.domain.Person;
+import Util.DBUtil;
 
 public class PersonDAO {
-	private PersonDAO personDAO = new PersonDAO();
+	
+	private static PersonDAO personDAO = new PersonDAO();
 	
 	private PersonDAO() {}
 	
-	public PersonDAO getPersonDAO() {
+	public static PersonDAO getPersonDAO() {
 		return personDAO;
 	}
 	
 	
 	// 사람 불러오기
-	public Person getPerson() {
-		return null;
+	public Person getPerson(String name) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		Person person = null;
+		try {
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement("select * from person where name=?");
+			pstmt.setString(1, name);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				person = new Person(rs.getInt(1), rs.getString(2));	
+			}
+		} finally {
+			DBUtil.close(conn, pstmt, rs);
+		}
+		
+		return person;
 	}
 
 	// 사람 저장
-	public boolean setPerson(String name) {
+	public boolean setPerson(String name) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement("insert into person (name) values (?)");
+			pstmt.setString(1, name);
+			
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} finally {
+			DBUtil.close(conn, pstmt);
+		}
+		
 		return false;
 	}
 }
