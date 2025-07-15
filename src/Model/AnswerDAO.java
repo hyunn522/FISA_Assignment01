ada5ad180ace737e8defc230fdbfca97caa55563
hyunn522 <tjgus52213@gmail.com>
2025-07-15 15:03:09 +0900

    feat: DB에서 정답 가져오는 메서드 추가

--- src/Model/AnswerDAO.java
+++ src/Model/AnswerDAO.java
@@ -1,19 +1,45 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import Model.domain.Answer;
+import Model.domain.Question;
+import Util.DBUtil;
 
 public class AnswerDAO {
-	private AnswerDAO answerDAO = new AnswerDAO();
+	
+	private static AnswerDAO answerDAO = new AnswerDAO();
 	
 	private AnswerDAO() {}
 	
-	public AnswerDAO getAnswerDAO() {
+	public static AnswerDAO getAnswerDao() {
 		return answerDAO;
 	}
 	
-	public ArrayList<Answer> getAnswers(){
-		return null;
+	// 질문에 대한 모든 정답 반환
+	public ArrayList<Answer> getAnswers() throws SQLException{
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		ArrayList<Answer> answers = null;
+		try {
+			conn = DBUtil.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select * from answer");
+			
+			answers = new ArrayList<>();
+			while (rs.next()) {
+				answers.add(new Answer(rs.getInt(1), rs.getInt(2), rs.getString(3)));
+			}
+		} finally {
+			DBUtil.close(conn, stmt);
+		}
+		
+		return answers;
 	}
 }
